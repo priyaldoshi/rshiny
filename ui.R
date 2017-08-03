f735b2fb78e6d8c016dda9ebb888c9c07991082c
priyaldoshi <30688310+priyaldoshi@users.noreply.github.com>
2017-08-03 14:58:32 +0530

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,29 @@
+ui <- dashboardPage(
+  dashboardHeader(title = "LPP"),
+  dashboardSidebar(
+    sidebarMenu(
+      # menuItemOutput("menuitem"),
+      selectInput("L1","L1 Category",names(table(attributes$L1)),selected=T,multiple = F),
+      selectInput("L4","L4 Categpry",names(table(attributes$L4)),selected=T,multiple = F)
+    )
+  ),
+  dashboardBody(
+    tabBox(width=1000,height=800,id = "conditionedPanels",
+           tabPanel("Choose Data",icon=icon("database"), 
+                    tableOutput('attributes2'),
+                    downloadButton('downloadData', 'Download'),
+                    tableOutput('lpp_sample1'),
+                    value = 1),
+           tabPanel("Linear Performance",icon=icon("cubes"),
+                    plotOutput('plot1',hover = "plot_hover"),
+                    verbatimTextOutput('equation'),
+                    verbatimTextOutput('rsquared'),
+                    tableOutput('anova'),
+                    value = 2)
+    )
+  )
+)
+
+
+shinyApp(ui, server)
+runGitHub(username = 'priyaldoshi',repo = 'rshiny')
